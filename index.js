fdd16d7b19b73a59225e105fee012ec09a9c6eb1
Henry Mortimer <henry@morti.plus.com>
2016-10-08 21:51:11 +0100

    add webhook method

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
 app.listen(port, function () {
 	console.log('Listening on port ' + port);
 });
