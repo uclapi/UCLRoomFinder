ccbc3d9806a86e64becdb2e6cc41494a34e253e8
Henry Mortimer <henry@morti.plus.com>
2016-10-08 22:35:52 +0100

    add config

--- index.js
+++ index.js
@@ -3,9 +3,35 @@ var app = express();
 
 var express = require('express');
 var app = express();
+var config = require('./config');
 
 var port = normalizePort(process.env.PORT || '8080');
 app.set('port', port);
+
+const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ? 
+  process.env.MESSENGER_APP_SECRET :
+  config.appSecret;
+
+// Arbitrary value used to validate a webhook
+const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+  (process.env.MESSENGER_VALIDATION_TOKEN) :
+  config.validationToken;
+
+// Generate a page access token for your page from the App Dashboard
+const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+  config.pageAccessToken;
+
+// URL where the app is running (include protocol). Used to point to scripts and 
+// assets located at this address. 
+const SERVER_URL = (process.env.SERVER_URL) ?
+  (process.env.SERVER_URL) :
+  config.serverURL;
+
+if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
+  console.error("Missing config values");
+  process.exit(1);
+}
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
